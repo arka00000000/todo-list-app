0f44b75d8239704f487bb8ba32298ae377dce7a2
arka00000000 <arka@cleonix.com>
2025-08-24 20:01:35 +0530

    Update TodoList.jsx
    Update TodoList.jsx

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/tasks";
+const API_URL ="https://todo-list-app-ykll.onrender.com/tasks";
 
 function TodoList() {
   const [tasks, setTasks] = useState([]);
